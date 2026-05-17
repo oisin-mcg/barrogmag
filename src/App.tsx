fd1bc3ee84d11c2ddc1b5feac6bcd706893d201b
oisin-mcg <oisin-mcg@users.noreply.github.com>
2026-05-17 13:54:37 +0100

    Remove admin redirect loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import SiteLayout from "./components/layout/SiteLayout";
 import AboutPage from "./pages/AboutPage";
@@ -6,6 +7,18 @@ import BlogPage from "./pages/BlogPage";
 import BlogPostPage from "./pages/BlogPostPage";
 import CurrentIssuePage from "./pages/CurrentIssuePage";
 import HomePage from "./pages/HomePage";
+
+function AdminFallback() {
+  useEffect(() => {
+    if (window.location.pathname !== "/admin/index.html") {
+      window.location.replace(
+        `/admin/index.html${window.location.search}${window.location.hash}`
+      );
+    }
+  }, []);
+
+  return null;
+}
 
 export default function App() {
   return (
@@ -18,6 +31,7 @@ export default function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/archive" element={<ArchivePage />} />
       </Route>
+      <Route path="/admin/*" element={<AdminFallback />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
